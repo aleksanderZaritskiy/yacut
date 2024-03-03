c768da3537e085173dfd318f4093991273c6e839
zaritskiyaa <zaritskiyaa@mail.ru>
2024-03-04 01:36:25 +0300

    flask YP project

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
